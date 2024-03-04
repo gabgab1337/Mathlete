1b487df5f4f210944e6ac5e31fa56faa31aabe24
Gabriel Hanszke <gabrielhanszke@gmail.com>
2024-03-04 03:52:02 +0100

    Moved buttons to diferent xml files

--- app/src/main/java/com/example/mathlete/MainActivity.kt
+++ app/src/main/java/com/example/mathlete/MainActivity.kt
@@ -1,43 +1,27 @@
 package com.example.mathlete
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.example.mathlete.ui.theme.MathleteTheme
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mathlete.R
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                setContentView(R.layout.scene_changer_layout)
-                val centerSceneText: TextView = findViewById(R.id.centerText)
-                val buttonGenerator: Button = findViewById(R.id.buttonGenerator)
-                buttonGenerator.setOnClickListener {
-                    centerSceneText.text = "Generator zadanek potężny"
-                }
-                val buttonLessons: Button = findViewById(R.id.buttonLessons)
-                buttonLessons.setOnClickListener {
-                    centerSceneText.text = "Lekcje tutaj o"
-                }
-                val buttonProgress: Button = findViewById(R.id.buttonProgress)
-                buttonProgress.setOnClickListener {
-                    centerSceneText.text = "Progressik"
-                }
-            }
+        setContentView(R.layout.main_screen_layout)
+        val centerSceneText: TextView = findViewById(R.id.centerText)
+        centerSceneText.text = "WITAJ W MATHLETE!"
+        val buttonGenerator: Button = findViewById(R.id.buttonGenerator)
+        buttonGenerator.setOnClickListener {
+
         }
+        val buttonLessons: Button = findViewById(R.id.buttonLessons)
+        buttonLessons.setOnClickListener {
+
+        }
+        val buttonProgress: Button = findViewById(R.id.buttonProgress)
+        buttonProgress.setOnClickListener {
+
+        }
+    }
+}
