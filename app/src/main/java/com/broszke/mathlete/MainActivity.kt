4beb3bcd62f7a4db10894cd8061fbbb8b0ada35d
Gabriel Hanszke <gabrielhanszke@gmail.com>
2024-03-24 23:39:42 +0100

    Added generator buttons

--- app/src/main/java/com/broszke/mathlete/MainActivity.kt
+++ app/src/main/java/com/broszke/mathlete/MainActivity.kt
@@ -2,6 +2,7 @@ package com.broszke.mathlete
 
 import android.os.Bundle
 import android.content.Intent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
@@ -11,18 +12,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
                 setContentView(R.layout.main_screen_layout)
                 val centerSceneText: TextView = findViewById(R.id.centerText)
-                val buttonGenerator: Button = findViewById(R.id.buttonGenerator)
-                buttonGenerator.setOnClickListener {
-                    val intent = Intent(this, QuizActivity::class.java)
-                    startActivity(intent)
-                }
+                centerSceneText.text = "Witaj w Mathlete!"
+
                 val buttonLessons: Button = findViewById(R.id.buttonLessons)
                 buttonLessons.setOnClickListener {
                     centerSceneText.text = "Lekcje tutaj o"
                 }
+
                 val buttonProgress: Button = findViewById(R.id.buttonProgress)
                 buttonProgress.setOnClickListener {
                     centerSceneText.text = "Progressik"
+                }
+
+                // Generator buttony
+                val buttonIds = arrayOf(
+                    R.id.buttonLinearGenerator,
+                    R.id.buttonQuadraticGenerator,
+                    R.id.button3Generator,
+                    R.id.button4Generator,
+                    R.id.button5Generator,
+                    R.id.button6Generator,
+                    R.id.button7Generator,
+                    R.id.button8Generator,
+                    R.id.button9Generator,
+                    R.id.button10Generator,
+                    R.id.button11Generator,
+                    R.id.button12Generator
+                )
+
+                val buttons = buttonIds.map { findViewById<Button>(it) }
+
+                val buttonGenerator: Button = findViewById(R.id.buttonGenerator)
+                buttonGenerator.setOnClickListener {
+                    centerSceneText.visibility = View.GONE
+                    buttons.forEach { it.visibility = View.VISIBLE }
+                    //val intent = Intent(this, QuizActivity::class.java)
+                    //startActivity(intent)
                 }
     }
 }
