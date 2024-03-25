1dd56f0bb676e553fa61e6944786ed72a2a73f50
Melior6 <dedeto34@gmail.com>
2024-03-25 14:14:13 +0100

    added ''zaloguj sie'' button in profile

--- app/src/main/java/com/broszke/mathlete/MainActivity.kt
+++ app/src/main/java/com/broszke/mathlete/MainActivity.kt
@@ -2,6 +2,7 @@ package com.broszke.mathlete
 
 import android.os.Bundle
 import android.content.Intent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
@@ -9,20 +10,27 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                setContentView(R.layout.main_screen_layout)
-                val centerSceneText: TextView = findViewById(R.id.centerText)
-                val buttonGenerator: Button = findViewById(R.id.buttonGenerator)
-                buttonGenerator.setOnClickListener {
-                    val intent = Intent(this, QuizActivity::class.java)
-                    startActivity(intent)
-                }
-                val buttonLessons: Button = findViewById(R.id.buttonLessons)
-                buttonLessons.setOnClickListener {
-                    centerSceneText.text = "Lekcje tutaj o"
-                }
-                val buttonProgress: Button = findViewById(R.id.buttonProgress)
-                buttonProgress.setOnClickListener {
-                    centerSceneText.text = "Progressik"
-                }
+        setContentView(R.layout.main_screen_layout)
+        val centerSceneText: TextView = findViewById(R.id.centerText)
+        val buttonGenerator: Button = findViewById(R.id.buttonGenerator)
+        buttonGenerator.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
+        val buttonLessons: Button = findViewById(R.id.buttonLessons)
+        buttonLessons.setOnClickListener {
+            centerSceneText.text = "Lekcje tutaj o"
+        }
+        val buttonProgress: Button = findViewById(R.id.buttonProgress)
+        buttonProgress.setOnClickListener {
+            centerSceneText.text = "Progressik"
+        }
+        val buttonLogin: Button = findViewById(R.id.buttonLogin)
+        val buttonProfile: Button = findViewById(R.id.buttonProfile)
+
+        buttonProfile.setOnClickListener{
+            buttonLogin.visibility = View.VISIBLE
+        }
+
     }
-}
+        }
